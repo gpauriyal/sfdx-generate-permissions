e7b93cab9e538f3e2520f1913036bf016a67f319
Girish <girish@Shalinis-MacBook-Pro-2.local>
2020-05-02 13:13:23 +1000

    v2.0

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const exec = require("child_process").execSync;
 const execAsync = require("child_process").exec;
 const chalk = require('chalk');
@@ -6,21 +8,11 @@ const clear = require('clear');
 const files = require('./lib/files');
 const utils = require('./lib/utils');
 
-//clear();
-// console.log(
-//     chalk.yellow(
-//         figlet.textSync('CRUD Generator', { horizontalLayout: 'half' })
-//     )
-// );
-// const tree = dirTree(process.argv[2], { extensions: /\.xml/ })
-
-// console.log(JSON.stringify(tree));
-// console.log(JSON.stringify(process.argv[2]));
 async function main() {
     try {
-        let profile = await utils.getFiles(process.argv[2], 'profiles');
+        let profile = await utils.getFiles(process.argv[2] + '/profiles');
         console.log(profile);
-        await utils.readFile(process.argv[2] + '/objects'
+        await utils.createPackage(process.argv[2] + '/objects/'
             , profile);
         console.log(chalk.green('finished generating printing xml'));
     } catch (e) {
