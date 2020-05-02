b7df72ebf778dac723265e12f02c4133d0f099f2
Girish <girish@Shalinis-MacBook-Pro-2.local>
2020-05-02 10:28:09 +1000

    v1.0

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const program = require('commander');
 const exec = require("child_process").execSync;
 const execAsync = require("child_process").exec;
-const dirTree = require("directory-tree");
 const chalk = require('chalk');
 const clear = require('clear');
-const figlet = require('figlet');
+// const figlet = require('figlet');
 const files = require('./lib/files');
 const utils = require('./lib/utils');
 
@@ -16,21 +14,17 @@ const utils = require('./lib/utils');
 // );
 // const tree = dirTree(process.argv[2], { extensions: /\.xml/ })
 
-const tree = dirTree(process.argv[2]);
-// utils.getFiles(tree, 'profiles');
 // console.log(JSON.stringify(tree));
 // console.log(JSON.stringify(process.argv[2]));
-// utils.getFiles(tree, 'profiles').then(function (result, err) {
-//     console.log(result);
-// }).catch(function (err) {
-//     console.log('Error' + err);
-
-// });
-
-
-console.log(JSON.stringify(tree));
-var profiles = [];
-
-function getMetadata() {
-
+async function main() {
+    try {
+        let profile = await utils.getFiles(process.argv[2], 'profiles');
+        console.log(profile);
+        await utils.readFile(process.argv[2] + '/objects'
+            , profile);
+        console.log(chalk.green('finished generating printing xml'));
+    } catch (e) {
+        console.log("Error", e);
+    }
 }
+main();
