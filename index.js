bb82a9897580a961b346210d743616ac41b04607
Girish <girish@Shalinis-MacBook-Pro-2.local>
2020-05-03 11:25:55 +1000

    v3.0

--- index.js
+++ index.js
@@ -2,11 +2,8 @@
 
 const exec = require("child_process").execSync;
 const execAsync = require("child_process").exec;
-const chalk = require('chalk');
-const clear = require('clear');
-// const figlet = require('figlet');
-const files = require('./lib/files');
 const utils = require('./lib/utils');
+
 
 async function main() {
     try {
@@ -14,9 +11,9 @@ async function main() {
         console.log(profile);
         await utils.createPackage(process.argv[2] + '/objects/'
             , profile);
-        console.log(chalk.green('finished generating printing xml'));
+        utils.log(('finished generating printing xml'));
     } catch (e) {
-        console.log("Error", e);
+        utils.error("Error " + e.message);
     }
 }
 main();
